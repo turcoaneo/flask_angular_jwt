6a71d4ed09dbd17f92437c7d9ad70432b6879127
Ovidiu Turcoane <ovidiu.turcoane@gmail.com>
2025-04-10 15:56:40 +0300

    Task : PROD deployment.

--- build_env.py
+++ build_env.py
@@ -11,8 +11,11 @@ DIST_PATH = os.path.join(ANGULAR_PROJECT_PATH, 'dist', directory, 'browser')
 FLASK_STATIC_PATH = os.path.join(CURRENT_DIRECTORY, 'static')
 FLASK_TEMPLATES_PATH = os.path.join(CURRENT_DIRECTORY, 'templates')
 
-env = os.getenv('ENVIRONMENT', 'uat')
-subprocess.call(('cd ' + ANGULAR_PROJECT_PATH + ' && ng build -c ' + env + ' --base-href /static/'), shell=True)
+env = os.getenv('ENVIRONMENT', ' ')
+if env != ' ':
+    env = f' -c {env} '
+command_build = f' && ng build{env}--base-href /static/'
+subprocess.call(('cd ' + ANGULAR_PROJECT_PATH + command_build), shell=True)
 
 
 def move_built_files(path, extension):
