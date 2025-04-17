b9c190a08566d028dd0ca93bbb55701040a764a1
Ovidiu Turcoane <ovidiu.turcoane@gmail.com>
2025-04-17 15:07:03 +0300

    Deployment : UAT.

--- static/chunk-NVYHQPRG.js
+++
@@ -1,1 +0,0 @@
-import{Pa as o,Qa as c,Wa as a,za as n}from"./chunk-53JJBBT3.js";var p=class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=n({type:e,selectors:[["app-contact"]],decls:2,vars:0,template:function(t,i){t&1&&(o(0,"p"),a(1,"contact works!"),c())},encapsulation:2})};export{p as ContactComponent};
