96eee638f6d51b39175144edab780a618b3441b8
Ovidiu Turcoane <ovidiu.turcoane@gmail.com>
2025-04-19 19:03:59 +0300

    SCRUM-11 : Configure-tests-for-Python. Test in UAT - FE.

---
+++ app/static/chunk-KKHEDYTD.js
@@ -0,0 +1,2 @@
+import{Pa as o,Qa as c,Wa as a,za as n}from"./chunk-D35P5UDE.js";var m=(()=>{class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-contact"]],decls:2,vars:0,template:function(e,p){e&1&&(o(0,"p"),a(1,"contact works!"),c())},encapsulation:2})}return t})();export{m as ContactComponent};
+//# sourceMappingURL=chunk-KKHEDYTD.js.map
