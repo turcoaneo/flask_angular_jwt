9143301308836fcde685a3c16f1fec2af72ac76a
Ovidiu Turcoane <ovidiu.turcoane@gmail.com>
2025-04-27 08:36:17 +0300

    SCRUM-13 #comment DOM examples.

--- angular-pumi/src/app/components/sign-in/sign-in.component.spec.ts
+++ angular-pumi/src/app/components/sign-in/sign-in.component.spec.ts
@@ -1,20 +1,26 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
 import {SignInComponent} from './sign-in.component';
 import {provideHttpClient} from '@angular/common/http';
 import {provideHttpClientTesting} from '@angular/common/http/testing';
 import {provideRouter} from '@angular/router';
+import {ControlContainer, FormsModule} from '@angular/forms';
+import {UserPassGroupComponent} from '../user-pass-group/user-pass-group.component';
+import {By} from '@angular/platform-browser';
 
 describe('SignInComponent', () => {
   let component: SignInComponent;
   let fixture: ComponentFixture<SignInComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      providers: [SignInComponent, provideHttpClient(), provideHttpClientTesting(), provideRouter([])]
-    })
-      .compileComponents();
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      providers: [SignInComponent, ControlContainer, UserPassGroupComponent,
+        provideHttpClient(), provideHttpClientTesting(), provideRouter([])],
+      imports: [FormsModule]
+    }).compileComponents();
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(SignInComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -22,5 +28,30 @@ describe('SignInComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show not valid form', async () => {
+    component.signInForm.setValue({
+      "signInUserPass": {
+        "signInEmail": "invalid email",
+        "signInPass": "user234&"
+      }
+    });
+
+    expect(component.signInForm.valid).toEqual(false);
+    let element = fixture.debugElement.query(By.css('p'));
+    const info = element.nativeElement;
+    expect(info.textContent.trim()).toContain('Create an account for one month free trial');
+  });
+
+  it('should show valid form', async () => {
+    component.signInForm.setValue({
+      "signInUserPass": {
+        "signInEmail": "user@user.ro",
+        "signInPass": "user234*"
+      }
+    });
+
+    expect(component.signInForm.valid).toEqual(true);
   });
 });
